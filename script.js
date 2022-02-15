91bf3f598814c35e7d2fbfa16aabd4cefa4a198a
DavGuetten <soulsec@protonmail.com>
2022-02-15 20:16:37 -0300

    Played with some things

--- script.js
+++ script.js
@@ -1,48 +1,24 @@
 console.log("Olá mundo");
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const playerSelection = [rockBtn, paperBtn, scissorsBtn];
 
 function computerPlay() {
-  let game = ["Rock", "Paper", "Scissors"];
-  return game[Math.floor(Math.random() * 3)];
+  let computerOptions = ["Rock", "Paper", "Scissors"];
+  let move = Math.floor(Math.random() * 3);
+  const computerSelection = computerOptions[move];
 }
 
-function playRound(playerSelection, computerSelection) {
-  const playerChoice = playerSelection.toLowerCase();
-  const computerChoice = computerSelection.toLowerCase();
-  if (playerChoice == "rock" && computerChoice == "rock") {
-    console.log("It's a draw!");
-  }
-  if (playerChoice == "rock" && computerChoice == "paper") {
-    console.log("You Lose! Paper beats Rock");
-  }
-  if (playerChoice == "rock" && computerChoice == "scissors") {
-    console.log("You Win! Rock beats Scissors");
-  }
-  if (playerChoice == "paper" && computerChoice == "rock") {
-    console.log("You Win! Paper beats Rock");
-  }
-  if (playerChoice == "paper" && computerChoice == "paper") {
-    console.log("It's a draw!");
-  }
-  if (playerChoice == "paper" && computerChoice == "scissors") {
-    console.log("You Lose! Scissors beats Paper");
-  }
-  if (playerChoice == "scissors" && computerChoice == "rock") {
-    console.log("You Lose! Rock beats Scissors");
-  }
-  if (playerChoice == "scissors" && computerChoice == "paper") {
-    console.log("You Win! Scissors beats Paper");
-  }
-  if (playerChoice == "scissors" && computerChoice == "scissors") {
-    console.log("It's a draw!");
-  }
+function playRound() {
+  playerSelection.forEach((selection) => {
+    selection.addEventListener("click", computerPlay);
+    console.log(playerSelection.innerText);
+  });
 }
 
 function game() {
-  for (i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
-  }
-  return "The game has ended";
+  let playerScore = 0;
+  let computerScore = 0;
 }
-const computerSelection = computerPlay();
-const playerSelection = prompt();
-console.log(game());
+console.log(playRound());

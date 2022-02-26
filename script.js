6b958c4ffd70c3ce0f37873d88440a9379f1f459
DavGuetten <soulsec@protonmail.com>
2022-02-26 11:56:46 -0300

    Made some changes to the logic

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 console.log("Olá mundo");
-const rockBtn = document.querySelector(".rock");
-const paperBtn = document.querySelector(".paper");
-const scissorsBtn = document.querySelector(".scissors");
+let playerScore = 0;
+let computerScore = 0;
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
 const playerSelection = [rockBtn, paperBtn, scissorsBtn];
 
 function computerPlay() {
-  let computerOptions = ["Rock", "Paper", "Scissors"];
+  const computerOptions = ["Rock", "Paper", "Scissors"];
   let move = Math.floor(Math.random() * 3);
   const computerSelection = computerOptions[move];
+  console.log(computerSelection);
 }
 
 function playRound() {
   playerSelection.forEach((selection) => {
     selection.addEventListener("click", computerPlay);
-    console.log(playerSelection.innerText);
+    console.log(selection.innerText);
   });
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-}
+function game() {}
 console.log(playRound());

c6809d01fcebd10c335a46e5d02fe48a33280a10
DavGuetten <soulsec@protonmail.com>
2022-03-29 14:45:40 -0300

    Some changes to the logic

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 let playerScore = 0;
 let computerScore = 0;
+let winner = "";
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
-const playerSelection = [rockBtn, paperBtn, scissorsBtn];
+const computerSelection = computerPlay();
 
-playerSelection.forEach((selection) => {
-  selection.addEventListener("click", playRound);
-});
+rockBtn.addEventListener("click", () => playRound("Rock", computerSelection));
+paperBtn.addEventListener("click", () => playRound("Paper", computerSelection));
+scissorsBtn.addEventListener("click", () =>
+  playRound("Scissors", computerSelection)
+);
 
 function computerPlay() {
-  const computerOptions = [rockBtn, paperBtn, scissorsBtn];
   let move = Math.floor(Math.random() * 3);
-  const computerSelection = computerOptions[move].innerText;
+  switch (move) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
 }
-computerPlay();
 
-function playRound(e) {
-  console.log(e.currentTarget.innerText);
-  // console.log(computerSelection);
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    winner = "tie";
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    playerScore++;
+    winner = "player";
+  } else {
+    computerScore++;
+    winner = "computer";
+  }
 }
 
 function game() {}

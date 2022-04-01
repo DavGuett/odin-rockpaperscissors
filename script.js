dbf0ff270a11daf3b16f1571b2cdae7899c4d12e
DavGuetten <soulsec@protonmail.com>
2022-04-01 15:07:38 -0300

    Only thing left is making it last a specific number of rounds

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-let playerScore = 0;
-let computerScore = 0;
-let winner = "";
-const rockBtn = document.querySelector("#rock");
-const paperBtn = document.querySelector("#paper");
-const scissorsBtn = document.querySelector("#scissors");
-const computerSelection = computerPlay();
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  const rockBtn = document.querySelector("#rock");
+  const paperBtn = document.querySelector("#paper");
+  const scissorsBtn = document.querySelector("#scissors");
+  const result = document.querySelector(".result");
+  let computerSelection;
+  const playerScoreCounter = document.querySelector(".player-score");
+  const computerScoreCounter = document.querySelector(".computer-score");
 
-rockBtn.addEventListener("click", () => playRound("Rock", computerSelection));
-paperBtn.addEventListener("click", () => playRound("Paper", computerSelection));
-scissorsBtn.addEventListener("click", () =>
-  playRound("Scissors", computerSelection)
-);
+  rockBtn.addEventListener("click", () => playRound("Rock", computerSelection));
+  paperBtn.addEventListener("click", () =>
+    playRound("Paper", computerSelection)
+  );
+  scissorsBtn.addEventListener("click", () =>
+    playRound("Scissors", computerSelection)
+  );
 
-function computerPlay() {
-  let move = Math.floor(Math.random() * 3);
-  switch (move) {
-    case 0:
-      return "Rock";
-    case 1:
-      return "Paper";
-    case 2:
-      return "Scissors";
+  function computerPlay() {
+    let move = Math.floor(Math.random() * 3);
+    switch (move) {
+      case 0:
+        return "Rock";
+      case 1:
+        return "Paper";
+      case 2:
+        return "Scissors";
+    }
+  }
+
+  function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    if (playerSelection === computerSelection) {
+      result.innerHTML = "It's a tie!";
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper") ||
+      (playerSelection === "Paper" && computerSelection === "Rock")
+    ) {
+      playerScore++;
+      result.innerHTML = `You win! ${playerSelection} beats ${computerSelection}`;
+      playerScoreCounter.innerHTML = playerScore;
+    } else {
+      computerScore++;
+      result.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}`;
+      computerScoreCounter.innerHTML = computerScore;
+    }
   }
 }
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    winner = "tie";
-  } else if (
-    (playerSelection === "Rock" && computerSelection === "Scissors") ||
-    (playerSelection === "Scissors" && computerSelection === "Paper") ||
-    (playerSelection === "Paper" && computerSelection === "Rock")
-  ) {
-    playerScore++;
-    winner = "player";
-  } else {
-    computerScore++;
-    winner = "computer";
-  }
-}
-
-function game() {}
+game();

1bd39517db8add16b707c2ecd4135aeec09d4ae6
DavGuetten <soulsec@protonmail.com>
2022-04-29 21:07:42 -0300

    added a div that shows the computer choice

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function game() {
   const paperBtn = document.querySelector("#paper");
   const scissorsBtn = document.querySelector("#scissors");
   const result = document.querySelector(".result");
+  const playerOption = document.querySelector(".player-option");
+  const computerOption = document.querySelector(".computer-option");
   let computerSelection;
   const playerScoreCounter = document.querySelector(".player-score");
   const computerScoreCounter = document.querySelector(".computer-score");
@@ -21,16 +23,26 @@ function game() {
     let move = Math.floor(Math.random() * 3);
     switch (move) {
       case 0:
+        computerOption.innerHTML = "✊";
         return "Rock";
       case 1:
+        computerOption.innerHTML = "✋";
         return "Paper";
       case 2:
+        computerOption.innerHTML = "✌";
         return "Scissors";
     }
   }
 
   function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
+    if (playerSelection === "Rock") {
+      playerOption.innerHTML = "✊";
+    } else if (playerSelection === "Paper") {
+      playerOption.innerHTML = "✋";
+    } else {
+      playerOption.innerHTML = "✌";
+    }
     if (playerSelection === computerSelection) {
       result.innerHTML = "It's a tie!";
     } else if (
